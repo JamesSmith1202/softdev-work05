7b57fefab8cccc5b4c5d2ec742482fbe48d60254
JamesSmith1202 <jsmith@stuy.edu>
2017-09-26 22:10:20 -0400

    did extra saucy stuff

--- work03.py
+++ work03.py
@@ -11,18 +11,21 @@ d = {}
 #returns a list of the csv file lines.
 def open_read(csvFile):
     csv_file = open(csvFile, 'r')
-    return csv_file.readlines()[1:len(csv_file.readlines())-1]#exlude the header and footer
+    return csv_file.readlines()[1:len(csv_file.readlines())-1]
 
 def lineToDictEntry(line):
     #This block deals with commas and splicing
     if line.find('''",''') == -1:
         job = line.split(",")[0]
         percentage = line.split(",")[1]
+        link = line.split(",")[2]
 
     else:
         job = line.split('''",''')[0][1:]
-        percentage = line.split('''",''')[1]
-    d[job] = float(percentage)
+        percentage = line.split('''",''')[1].split(",")[0]
+        link = line.split('''",''')[1].split(",")[1]
+    numLinkTuple = (float(percentage),link)
+    d[job] = numLinkTuple
     return d
 
 #Turns the csv line list into a dictionary with Profession:Percentage
@@ -36,17 +39,10 @@ def getRandom():
     threshold = random.random() * 99.8#total percentage
     counter = 0
     for entry in d:
-        counter += d[entry]
+        counter += d[entry][0]
         if(counter > threshold):
             return entry
 #CORE FUNCTIONS ===========================================================================
-
-#TESTING FUNCTIONS ========================================================================
-def printAll():
-    for entry in d:
-        print entry, d[entry]
-
-#TESTING FUNCTIONS ========================================================================
 
 #testing program
 def run(csvFile):
